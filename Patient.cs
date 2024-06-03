0e06503a10b5c375bdca42d3ac8212f736bb7c68
morgan <morgan.dagron@gmail.com>
2024-06-03 12:22:46 +0200

    Ajout patient, DAOAuthentification, DAOPatient, DAOVisite

---
+++ Patient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _projet_hopital
+{
+    class Patient
+    {
+        private int id;
+        private string prenom;
+        private string nom; 
+        private int age;
+        private string adresse;
+        private int telephone;
+
+        public Patient() { }
+
+
+        public override string ToString()
+        {
+            string res = $"Patient : {nom} {prenom} (identifiant n° {id})";
+            res = $"\nAdresse : {adresse} - Téléphone : {telephone}" ;
+            return "Patient :";
+        }
+    }
+}
