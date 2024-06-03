35b43b6ebc5cc32cc090d60e3655b27d673ecd3a
Sytreza <antoine.furet.96@gmail.com>
2024-06-03 13:42:56 +0200

    Create Class Visite

---
+++ Visite.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _projet_hopital
+{
+    class Visite
+    {
+        public int Id { get; }
+        public int IdPatient { get; set; }
+        public string NomMedecin { get; set; }
+        public decimal CoutVisite { get; set; }
+        public DateTime DateVisite { get; set; }
+        public int NumSalle { get; set; }
+
+        public Visite()
+        {
+        }
+
+        public Visite(int idPatient, string nomMedecin, DateTime dateVisite, int numSalle)
+        {
+            IdPatient = idPatient;
+            NomMedecin = nomMedecin;
+            DateVisite = dateVisite;
+            NumSalle = numSalle;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, IdPatient: {IdPatient}, NomMedecin: {NomMedecin}, CoutVisite: {CoutVisite}, DateVisite: {DateVisite}, NumSalle: {NumSalle}";
+        }
+    }
+}
