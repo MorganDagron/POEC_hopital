9aea9d5b5ceebe55d445a12a2cb5a659465788d7
Sytreza <antoine.furet.96@gmail.com>
2024-06-03 15:52:10 +0200

    DAO Authentification-Patient-Visite

--- DAOAuthentification.cs
+++ DAOAuthentification.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,106 @@ namespace _projet_hopital
 {
     class DAOAuthentification
     {
+        SqlConnection connection = new SqlConnection(ConnectionString.ConnexionBdd);
+
+        public List<Authentification> GetAllAuthentifications()
+        {
+            List<Authentification> authentifications = new List<Authentification>();
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM authentification", connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Authentification authentification = new Authentification
+                {
+                    Login = (string)reader["login"],
+                    Password = (string)reader["password"],
+                    Nom = (string)reader["nom"],
+                    Metier = (int)reader["metier"]
+                };
+                authentifications.Add(authentification);
+            }
+
+            reader.Close();
+            connection.Close();
+
+            return authentifications;
+        }
+
+        public Authentification GetAuthentificationByLogin(string login)
+        {
+            Authentification authentification = null;
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM authentification WHERE login = @login", connection);
+            command.Parameters.AddWithValue("@login", login);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                authentification = new Authentification
+                {
+                    Login = (string)reader["login"],
+                    Password = (string)reader["password"],
+                    Nom = (string)reader["nom"],
+                    Metier = (int)reader["metier"]
+                };
+            }
+
+            reader.Close();
+            connection.Close();
+
+            return authentification;
+
+        }
+
+        public void InsertAuthentification(Authentification authentification)
+        {
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("INSERT INTO authentification (login, password, nom, metier) VALUES (@login, @password, @nom, @metier)", connection);
+            command.Parameters.AddWithValue("@login", authentification.Login);
+            command.Parameters.AddWithValue("@password", authentification.Password);
+            command.Parameters.AddWithValue("@nom", authentification.Nom);
+            command.Parameters.AddWithValue("@metier", authentification.Metier);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+
+        public void UpdateAuthentification(Authentification authentification)
+        {
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("UPDATE authentification SET password = @password, nom = @nom, metier = @metier WHERE login = @login", connection);
+            command.Parameters.AddWithValue("@login", authentification.Login);
+            command.Parameters.AddWithValue("@password", authentification.Password);
+            command.Parameters.AddWithValue("@nom", authentification.Nom);
+            command.Parameters.AddWithValue("@metier", authentification.Metier);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void DeleteAuthentification(string login)
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM authentification WHERE login = @login", connection);
+            command.Parameters.AddWithValue("@login", login);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
     }
 }
+
--- DAOPatient.cs
+++ DAOPatient.cs
@@ -1,12 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace _projet_hopital
 {
     class DAOPatient
     {
+        SqlConnection connection = new SqlConnection(ConnectionString.ConnexionBdd);
+
+        public List<Patient> GetAllPatients()
+        {
+            List<Patient> patients = new List<Patient>();
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM patients", connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Patient patient = new Patient
+                {
+                    Id = (int)reader["id"],
+                    Nom = (string)reader["nom"],
+                    Prenom = (string)reader["prenom"],
+                    Age = (int)reader["age"],
+                    Adresse = (string)reader["adresse"],
+                    Telephone = (string)reader["telephone"]
+                };
+
+                patients.Add(patient);
+            }
+
+            reader.Close();
+            connection.Close();
+            return patients;
+        }
+
+        public Patient GetPatientById(int id)
+        {
+            Patient patient = null;
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM patients WHERE id = @id", connection);
+            command.Parameters.AddWithValue("@id", id);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                patient = new Patient
+                {
+                    Id = (int)reader["id"],
+                    Nom = (string)reader["nom"],
+                    Prenom = (string)reader["prenom"],
+                    Age = (int)reader["age"],
+                    Adresse = (string)reader["adresse"],
+                    Telephone = (string)reader["telephone"]
+                };
+            }
+
+            reader.Close();
+            connection.Close();
+            return patient;
+        }
+
+        public void InsertPatient(Patient patient)
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("INSERT INTO patients (nom, prenom, age, adresse, telephone) VALUES (@nom, @prenom, @age, @adresse, @telephone)", connection);
+            command.Parameters.AddWithValue("@nom", patient.Nom);
+            command.Parameters.AddWithValue("@prenom", patient.Prenom);
+            command.Parameters.AddWithValue("@age", patient.Age);
+            command.Parameters.AddWithValue("@adresse", patient.Adresse);
+            command.Parameters.AddWithValue("@telephone", patient.Telephone);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        public void UpdatePatient(Patient patient)
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("UPDATE patients SET nom = @nom, prenom = @prenom, age = @age, adresse = @adresse, telephone = @telephone WHERE id = @id", connection);
+            command.Parameters.AddWithValue("@nom", patient.Nom);
+            command.Parameters.AddWithValue("@prenom", patient.Prenom);
+            command.Parameters.AddWithValue("@age", patient.Age);
+            command.Parameters.AddWithValue("@adresse", patient.Adresse);
+            command.Parameters.AddWithValue("@telephone", patient.Telephone);
+            command.Parameters.AddWithValue("@id", patient.Id);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
+        }
+
+        public void DeletePatient(int id)
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM patients WHERE id = @id", connection);
+            command.Parameters.AddWithValue("@id", id);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
     }
 }
